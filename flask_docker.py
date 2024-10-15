b43ca2495411f375c8570c633a563b65937b0c41
HattoriHoa <hoab2205983@student.ctu.edu.vn>
2024-10-15 23:49:23 +0700

    second commit

--- flask_docker.py
+++ flask_docker.py
@@ -9,10 +9,3 @@ if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')from flask import Flask
 app = Flask(__name__)
 
-
-@app.route('/')
-def hello_world():
-    return 'Hello FOSS'
-
-if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0')
